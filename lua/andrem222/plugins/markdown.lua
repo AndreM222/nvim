95c7220240bb8da43965f8ccd3d6babeb99afd10
Andre Mossi <mossiroberto0392@gmail.com>
2025-01-25 21:14:38 -0500

    fix(markdview update): make changes accoridng to markview new update

--- lua/andrem222/plugins/markdown.lua
+++ lua/andrem222/plugins/markdown.lua
@@ -7,19 +7,22 @@ return {
     {
         "OXY2DEV/markview.nvim",
         lazy = true,      -- Recommended
-        ft = {"markdown", "tex"}, -- If you decide to lazy-load anyway
+        ft = "markdown", -- If you decide to lazy-load anyway
         config = function ()
 
             require("markview").setup({
-                modes = { "n", "i", "no", "c" },
-                hybrid_modes = { "i", "n" },
+                preview = {
+                    modes = { "n", "i", "no", "c" },
 
-                -- This is nice to have
-                callbacks = {
-                    on_enable = function (_, win)
-                        vim.wo[win].conceallevel = 2;
-                        vim.wo[win].concealcursor = "nc";
-                    end
+                    hybrid_modes = { "i", "n" },
+
+                    -- This is nice to have
+                    callbacks = {
+                        on_enable = function (_, win)
+                            vim.wo[win].conceallevel = 2;
+                            vim.wo[win].concealcursor = "nc";
+                        end
+                    }
                 },
                 checkboxes = {
                     enable = true,
@@ -27,150 +30,124 @@ return {
                     checked = {
                         text = "󰡖", hl = "MarkviewCheckboxChecked"
                     },
+
                     unchecked = {
                         text = "", hl = "MarkviewCheckboxUnchecked"
                     }
                 },
+
                 links = {
-                    enable = true,
-
-                    images = {
-                        icon = " ",
-                        hl = "MarkviewImageLink",
-                    }
+                    enable = true
                 },
+
                 highlight_groups = {
-                    {
-                        output = function (hl)
-                            return {
-                                group_name = "MarkviewHeading1Corner",
-                                value = { fg = hl.hex(hl.color("bg", {  "MarkviewHeading1" }, nil, nil)) }
-                            }
+                    -- The property name is ZZ because we want to load this last.
+                    ["ZZ"] = function ()
+                        local hl = require("markview.highlights");
+
+                        local _o = {};
+
+                        for h = 1, 6, 1 do
+                            -- `hl.color()` → `hl.get_property()` as it can now get highlight group properties too
+                            local bg = hl.get_property("bg", { "MarkviewHeading" .. h }, nil, nil);
+
+                            if bg then
+                                table.insert(_o, {
+                                    group_name = "MarkviewHeading" .. h .. "Corner",
+                                    -- `hl.hex()` → `hl.rgb_to_hex()` as the previous name wasn't obvious(because there's now HSL & Lab color functions too).
+                                    value = { fg = hl.rgb_to_hex(bg) }
+                                });
+                            end
                         end
-                    },
-                    {
-                        output = function (hl)
-                            return {
-                                group_name = "MarkviewHeading2Corner",
-                                value = { fg = hl.hex(hl.color("bg", {  "MarkviewHeading2" }, nil, nil)) }
-                            }
-                        end
-                    },
-                    {
-                        output = function (hl)
-                            return {
-                                group_name = "MarkviewHeading3Corner",
-                                value = { fg = hl.hex(hl.color("bg", {  "MarkviewHeading3" }, nil, nil)) }
-                            }
-                        end
-                    },
-                    {
-                        output = function (hl)
-                            return {
-                                group_name = "MarkviewHeading4Corner",
-                                value = { fg = hl.hex(hl.color("bg", {  "MarkviewHeading4" }, nil, nil)) }
-                            }
-                        end
-                    },
-                    {
-                        output = function (hl)
-                            return {
-                                group_name = "MarkviewHeading5Corner",
-                                value = { fg = hl.hex(hl.color("bg", {  "MarkviewHeading5" }, nil, nil)) }
-                            }
-                        end
-                    },
-                    {
-                        output = function (hl)
-                            return {
-                                group_name = "MarkviewHeading6Corner",
-                                value = { fg = hl.hex(hl.color("bg", {  "MarkviewHeading6" }, nil, nil)) }
-                            }
-                        end
-                    }
+
+                        return _o;
+                    end
                 },
-                headings = {
-                    enable = true,
-                    shift_width = 0,
 
-                    heading_1 = {
-                        style = "label",
+                markdown = {
+                    headings = {
+                        enable = true,
+                        shift_width = 0,
 
-                        padding_left = " ",
-                        padding_right = " ",
+                        heading_1 = {
+                            style = "label",
 
-                        corner_left = "",
-                        corner_left_hl = "MarkviewHeading1Corner",
-                        corner_right = "",
-                        corner_right_hl = "MarkviewHeading1Corner",
+                            padding_left = " ",
+                            padding_right = " ",
 
-                        hl = "MarkviewHeading1"
-                    },
-                    heading_2 = {
-                        style = "label",
+                            corner_left = "",
+                            corner_left_hl = "MarkviewHeading1Corner",
+                            corner_right = "",
+                            corner_right_hl = "MarkviewHeading1Corner",
 
-                        padding_left = " ",
-                        padding_right = " ",
+                            hl = "MarkviewHeading1"
+                        },
+                        heading_2 = {
+                            style = "label",
 
-                        corner_left = "",
-                        corner_left_hl = "MarkviewHeading2Corner",
-                        corner_right = "",
-                        corner_right_hl = "MarkviewHeading2Corner",
+                            padding_left = " ",
+                            padding_right = " ",
 
-                        hl = "MarkviewHeading2"
-                    },
-                    heading_3 = {
-                        style = "label",
+                            corner_left = "",
+                            corner_left_hl = "MarkviewHeading2Corner",
+                            corner_right = "",
+                            corner_right_hl = "MarkviewHeading2Corner",
 
-                        padding_left = " ",
-                        padding_right = " ",
+                            hl = "MarkviewHeading2"
+                        },
+                        heading_3 = {
+                            style = "label",
 
-                        corner_left = "",
-                        corner_left_hl = "MarkviewHeading3Corner",
-                        corner_right = "",
-                        corner_right_hl = "MarkviewHeading3Corner",
+                            padding_left = " ",
+                            padding_right = " ",
 
-                        hl = "MarkviewHeading3"
-                    },
-                    heading_4 = {
-                        style = "label",
+                            corner_left = "",
+                            corner_left_hl = "MarkviewHeading3Corner",
+                            corner_right = "",
+                            corner_right_hl = "MarkviewHeading3Corner",
 
-                        padding_left = " ",
-                        padding_right = " ",
+                            hl = "MarkviewHeading3"
+                        },
+                        heading_4 = {
+                            style = "label",
 
-                        corner_left = "",
-                        corner_left_hl = "MarkviewHeading4Corner",
-                        corner_right = "",
-                        corner_right_hl = "MarkviewHeading4Corner",
+                            padding_left = " ",
+                            padding_right = " ",
 
-                        hl = "MarkviewHeading4"
-                    },
-                    heading_5 = {
-                        style = "label",
+                            corner_left = "",
+                            corner_left_hl = "MarkviewHeading4Corner",
+                            corner_right = "",
+                            corner_right_hl = "MarkviewHeading4Corner",
 
-                        padding_left = " ",
-                        padding_right = " ",
+                            hl = "MarkviewHeading4"
+                        },
+                        heading_5 = {
+                            style = "label",
 
-                        corner_left = "",
-                        corner_left_hl = "MarkviewHeading5Corner",
-                        corner_right = "",
-                        corner_right_hl = "MarkviewHeading5Corner",
+                            padding_left = " ",
+                            padding_right = " ",
 
-                        hl = "MarkviewHeading5"
-                    },
-                    heading_6 = {
-                        style = "label",
+                            corner_left = "",
+                            corner_left_hl = "MarkviewHeading5Corner",
+                            corner_right = "",
+                            corner_right_hl = "MarkviewHeading5Corner",
 
-                        padding_left = " ",
-                        padding_right = " ",
+                            hl = "MarkviewHeading5"
+                        },
+                        heading_6 = {
+                            style = "label",
 
-                        corner_left = "",
-                        corner_left_hl = "MarkviewHeading6Corner",
-                        corner_right = "",
-                        corner_right_hl = "MarkviewHeading6Corner",
+                            padding_left = " ",
+                            padding_right = " ",
 
-                        hl = "MarkviewHeading6"
-                    },
+                            corner_left = "",
+                            corner_left_hl = "MarkviewHeading6Corner",
+                            corner_right = "",
+                            corner_right_hl = "MarkviewHeading6Corner",
+
+                            hl = "MarkviewHeading6"
+                        },
+                    }
                 }
             })
         end
