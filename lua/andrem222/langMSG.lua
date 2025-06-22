460cac3a12621196180156c081929fcce568ac8d
Andre Mossi <mossiroberto0392@gmail.com>
2025-06-22 09:03:27 -0400

    feat(multiline and update): handle well updates for delete and add, and multiline translation

--- lua/andrem222/langMSG.lua
+++ lua/andrem222/langMSG.lua
@@ -10,17 +10,48 @@ local function parse_po(path)
     local file = io.open(path, "r")
     if not file then return {} end
 
-    local msgid, msgstr
+    local translations = {}
+    local mode = nil
+    local current_msgid = {}
+    local current_msgstr = {}
+
+    local function unescape(str)
+        return str:gsub('\\"', '"'):gsub("\\n", "\n")
+    end
+
     for line in file:lines() do
-        if line:match("^msgid") then
-            msgid = line:match('^msgid%s+"(.*)"')
-        elseif line:match("^msgstr") then
-            msgstr = line:match('^msgstr%s+"(.*)"')
-            if msgid and msgstr then
-                translations[msgid] = msgstr
-                msgid, msgstr = nil, nil
+        if line:match('^msgid%s+"') then
+            mode = "msgid"
+            current_msgid = { line:match('^msgid%s+"(.*)"') }
+            current_msgstr = {}
+        elseif line:match('^msgstr%s+"') then
+            mode = "msgstr"
+            current_msgstr = { line:match('^msgstr%s+"(.*)"') }
+        elseif line:match('^"') then
+            local str = line:match('^"(.*)"')
+            if mode == "msgid" then
+                table.insert(current_msgid, str)
+            elseif mode == "msgstr" then
+                table.insert(current_msgstr, str)
             end
+        elseif line == "" then
+            -- End of one entry
+            if #current_msgid > 0 and #current_msgstr > 0 then
+                local msgid_text = unescape(table.concat(current_msgid))
+                local msgstr_text = unescape(table.concat(current_msgstr))
+                translations[msgid_text] = msgstr_text
+            end
+            mode = nil
+            current_msgid = {}
+            current_msgstr = {}
         end
+    end
+
+    -- Handle the last entry if file doesn't end with blank line
+    if #current_msgid > 0 and #current_msgstr > 0 then
+        local msgid_text = unescape(table.concat(current_msgid))
+        local msgstr_text = unescape(table.concat(current_msgstr))
+        translations[msgid_text] = msgstr_text
     end
 
     file:close()
