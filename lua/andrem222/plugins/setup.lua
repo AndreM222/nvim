3f61fe6fc61db6f073f537393df05cc8a5cf9792
Andre Mossi <mossiroberto0392@gmail.com>
2025-08-05 22:01:16 -0400

    feat(vim ts): add vim treesitter

--- lua/andrem222/plugins/setup.lua
+++ lua/andrem222/plugins/setup.lua
@@ -210,6 +210,7 @@ return {
                     "latex",
                     "python",
                     "javascript",
+                    "vim",
                     "vimdoc",
                     "regex",
                     (function()
